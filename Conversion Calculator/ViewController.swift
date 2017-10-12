348b1ce77b1ca43a26c83eba3b411706e7f1ebcc
AbdulEldarrat <aoe7x5@mail.missouri.edu>
2017-10-11 20:02:20 -0500

    added new methods for converting

--- Conversion Calculator/ViewController.swift
+++ Conversion Calculator/ViewController.swift
@@ -19,10 +19,35 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
     // Function for converting volume
+    // foot-inches
+    func feetToInches(feet:Double)->Double{
+        return feet/12.0
+    }
+        //inces-foot
+    func inchesToFeet(inches:Double)->Double{
+        return inches*(0.083)
+    }
+    //kilometers-miles
+    func KmToMiles(km:Double)->Double{
+        return km*0.621371
+    }
+    func milesToKm(miles:Double)->Double{
+        return miles*1.60934
+    }
+    
+    //miles-kilometers
+    func metersToKm(meters:Double)->Double{
+        return meters*0.001
+    }
+    func kmToMeters(km:Double)->Double{
+        return km/1000
+    }
+    
     // Function for converting Length
+    
     // Function for converting Speed
+    
     // Function for converting Temp
     // Function for converting weight
 }
